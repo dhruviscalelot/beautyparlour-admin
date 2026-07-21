6951e08af8d3e4423fd8e89c3cb1757fec663129
dhruviscalelot <dhruviscalelot@gmail.com>
2026-07-21 12:08:32 +0530

    login page in toast message add

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -5,6 +5,7 @@ import { useNavigate } from 'react-router-dom'
 import { EMAIL_VALIDATION, PASSWORD_VALIDATION } from '../common/ErrorMessageCommom'
 import { assets } from '../assets/Images/assets'
 import { Eye, EyeOff } from "lucide-react";
+import toast from 'react-hot-toast'
 
 const STATIC_LOGIN = {
   email: 'admin@beautyparlour.com',
@@ -29,6 +30,7 @@ const Login = () => {
     if (values.email === STATIC_LOGIN.email && values.password === STATIC_LOGIN.password) {
       // localStorage.setItem('accessToken', 'static-admin-token')
       // localStorage.setItem('adminEmail', values.email)
+      toast.success("Admin logged in successfully!");
       navigate('/dashboard')
     } else {
       alert('Invalid email or password')
@@ -70,7 +72,7 @@ const Login = () => {
               <div className="w-16 h-16 md:w-20 md:h-20 mb-5 rounded-2xl bg-l3 border border-l2 shadow-[0_16px_35px_rgba(183,110,121,0.22)] flex items-center justify-center">
                 <span className="font-Prata text-30 md:text-36 text-primary leading-none">
                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                   stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-flower2 lucide-flower-2 h-7 w-7" aria-hidden="true"><path d="M12 5a3 3 0 1 1 3 3m-3-3a3 3 0 1 0-3 3m3-3v1M9 8a3 3 0 1 0 3 3M9 8h1m5 0a3 3 0 1 1-3 3m3-3h-1m-2 3v-1"></path><circle cx="12" cy="8" r="2"></circle><path d="M12 10v12"></path><path d="M12 22c4.2 0 7-1.667 7-5-4.2 0-7 1.667-7 5Z"></path><path d="M12 22c-4.2 0-7-1.667-7-5 4.2 0 7 1.667 7 5Z"></path></svg>
+                    stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-flower2 lucide-flower-2 h-7 w-7" aria-hidden="true"><path d="M12 5a3 3 0 1 1 3 3m-3-3a3 3 0 1 0-3 3m3-3v1M9 8a3 3 0 1 0 3 3M9 8h1m5 0a3 3 0 1 1-3 3m3-3h-1m-2 3v-1"></path><circle cx="12" cy="8" r="2"></circle><path d="M12 10v12"></path><path d="M12 22c4.2 0 7-1.667 7-5-4.2 0-7 1.667-7 5Z"></path><path d="M12 22c-4.2 0-7-1.667-7-5 4.2 0 7 1.667 7 5Z"></path></svg>
                 </span>
               </div>
               <h1 className="text-30 md:text-40 2xl:text-48 font-Prata text-g1 leading-tight mb-2">
