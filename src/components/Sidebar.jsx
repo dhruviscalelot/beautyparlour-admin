42bbe73b5c733e423fef1208dd174ded3bae892c
dhruviscalelot <dhruviscalelot@gmail.com>
2026-07-21 10:58:22 +0530

    sidebar in mobile responsive for the icon implement and svg logo error resolved

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,6 +2,7 @@ import { NavLink } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 import { useToggleSideBar, useToggleSideBarMobile } from '../Store/Selectors/Sidebar/Sidebar_Selectors'
 import { setSidebarOpenMobile } from '../Store/Action/Sidebar/Sidebar_Action'
+import { ChevronLeft } from 'lucide-react'
 
 function Sidebar({ MainMenu = [] }) {
     const dispatch = useDispatch()
@@ -23,10 +24,20 @@ function Sidebar({ MainMenu = [] }) {
             )}
 
             <aside className={`sidebar_wrap ${isOpenMobile ? 'sidebar_open' : ''} ${isExpanded ? '' : 'sidebar_collapsed'}`}>
+                <div 
+                    className={`w-7 h-7 rounded-full bg-primary flex lg:hidden items-center justify-center absolute left-60 top-20 z-50 cursor-pointer transition-opacity duration-300 ${
+                        isOpenMobile ? 'opacity-100' : 'opacity-0 pointer-events-none'
+                    }`} 
+                    onClick={() => { dispatch(setSidebarOpenMobile(false)) }}
+                >
+                    <span className="text-[16px] lg:text-[18px] 2xl:text-[20px] text-white">
+                        <ChevronLeft size={20} />
+                    </span>
+                </div>
                 <div className="sidebar_logo">
                     <div className="sidebar_logo_icon">
                         <span className="font-Prata text-24 2xl:text-28 text-primary leading-none">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-flower2 lucide-flower-2 h-5 w-5" aria-hidden="true"><path d="M12 5a3 3 0 1 1 3 3m-3-3a3 3 0 1 0-3 3m3-3v1M9 8a3 3 0 1 0 3 3M9 8h1m5 0a3 3 0 1 1-3 3m3-3h-1m-2 3v-1"></path><circle cx="12" cy="8" r="2"></circle><path d="M12 10v12"></path><path d="M12 22c4.2 0 7-1.667 7-5-4.2 0-7 1.667-7 5Z"></path><path d="M12 22c-4.2 0-7-1.667-7-5 4.2 0 7 1.667 7 5Z"></path></svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-flower2 lucide-flower-2 h-5 w-5" aria-hidden="true"><path d="M12 5a3 3 0 1 1 3 3m-3-3a3 3 0 1 0-3 3m3-3v1M9 8a3 3 0 1 0 3 3M9 8h1m5 0a3 3 0 1 1-3 3m3-3h-1m-2 3v-1"></path><circle cx="12" cy="8" r="2"></circle><path d="M12 10v12"></path><path d="M12 22c4.2 0 7-1.667 7-5-4.2 0-7 1.667-7 5Z"></path><path d="M12 22c-4.2 0-7-1.667-7-5 4.2 0 7 1.667 7 5Z"></path></svg>
                         </span>
                     </div>
                     <div className="sidebar_logo_text ml-3">
